9a26a4883756d187c4bf2de5ab070b8eba6aee80
chendie <chendie>
2023-04-09 14:50:41 +0800

    12

--- config/paths.js
+++ config/paths.js
@@ -21,7 +21,7 @@ const publicUrlOrPath = getPublicUrlOrPath(
   process.env.PUBLIC_URL
 );
 
-const buildPath = process.env.BUILD_PATH || '/root/nginx/html/blog-admin';
+const buildPath = process.env.BUILD_PATH || 'blog-admin';
 
 const moduleFileExtensions = [
   'web.mjs',
